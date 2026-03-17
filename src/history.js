e28f95daa712cbe1119416944702782fc207cd53
Matthias Kestenholz <mk@feinheit.ch>
2026-03-17 15:20:19 +0100

    Slightly improve the icon (in my opinion)

--- src/history.js
+++ src/history.js
@@ -1,4 +1,5 @@
 import { UndoRedo } from "@tiptap/extensions"
+
 export { UndoRedo }
 // Rename back to old export
 export const History = UndoRedo.extend({ name: "history" })
--- src/table.js
+++ src/table.js
@@ -195,9 +195,13 @@ function defineTableMenuItems({ buttons, menu }) {
     buttons.svg(
       `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <rect x="3" y="3" width="18" height="18" rx="2"/>
-      <line x1="9" y1="9" x2="15" y2="15"/>
-      <line x1="15" y1="9" x2="9" y2="15"/>
-      <rect x="3" y="3" width="18" height="18" rx="2" fill="#F44336" fill-opacity="0.3" stroke="#F44336"/>
+      <line x1="3" y1="9" x2="21" y2="9"/>
+      <line x1="9" y1="3" x2="9" y2="21"/>
+      <line x1="15" y1="3" x2="15" y2="21"/>
+      <line x1="3" y1="15" x2="21" y2="15"/>
+      <rect x="3" y="3" width="18" height="18" rx="2" fill="#F44336" fill-opacity="0.2" stroke="#F44336" stroke-width="1.5"/>
+      <line x1="6" y1="6" x2="18" y2="18" stroke="#F44336" stroke-width="2"/>
+      <line x1="18" y1="6" x2="6" y2="18" stroke="#F44336" stroke-width="2"/>
     </svg>`,
       "Delete table",
     ),
